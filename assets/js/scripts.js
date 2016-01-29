b167e72e4d4555165fe68d0b4b32fd8b0af94dfa
Chyngyz Arystan uulu <camphosis@gmail.com>
2016-01-30 00:24:01 +0600

    Home page done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,6 @@
     var routing = function (mode) {
       router = new Navigo(null, mode === 'hash');
       router.on({
-        
         'services/:service': function (params) {
           setContent(null, params.service);
         },
@@ -23,18 +22,13 @@
         }
       });
       router.on(function() {
-        setContent('about');
+        aboutFn();
       });
     };
 
-    var setc = function () {
-      $('.js-content').html('Hello');
-    }
-
 
     // Content Loader
-    var setContent = function(id, content) {
-
+    function setContent(id, content) {
       var slug = id || 'services-' + content;
       $('.js-content').load('partial/' + slug + '.html', function () {
         console.log('Loaded ' + slug);
@@ -42,7 +36,7 @@
     };
 
     // Main navigation
-    var navigation = function () {
+    function navigation () {
       []
       .slice
       .call(document.querySelectorAll('nav a'))
@@ -55,13 +49,350 @@
       });
     };
 
+
+    // Close loading
+    var Loader = function () {
+      var loader = $('.m-app-loading');
+      var svg = $(loader).find('svg');
+      var loaderLine = $(loader).find('.load-wrap');
+
+      //init();
+      return {
+        hide: hide,
+        show: show
+      }
+
+      function init () {
+        TweenMax.set(loaderLine, {
+          opacity: 0
+        });
+        TweenMax.set(svg, {
+          scale:0
+        });
+        TweenMax.set(loader, {
+          opacity:0
+        });
+      }
+
+      function hide () {
+        var tl = new TimelineLite;
+        tl.to(loaderLine, 0.3, {
+          opacity: 0
+        }).to(svg, .5, {
+          scale: 0,
+          ease:Back.easeOut
+        }).to(loader, 0.2, {
+          opacity: 0,
+          onComplete: function () {
+            $(loader).css('visibility', 'hidden');
+            console.log('Preload closed');
+          }
+        });
+      }
+
+      function show () {
+        var tl = new TimelineLite;
+        $(loader).css('visibility', 'visible');
+        tl.to(loader, 0.2, {
+          opacity: 1
+        }).to(svg, .3, {
+          scale: 1,
+          ease:Back.easeOut
+        }).to(loaderLine, 0.3, {
+          opacity: 1,
+          onComplete: function () {
+            console.log('Preloader opened');
+          }
+        });
+      }
+    }
+
+    var loader = new Loader();
+
+    function menuTrigger () {
+      $('.burger').click(function () {
+        $(this).toggleClass('open');
+        openMenu(true);
+      })
+    }
+
+    // Audio vars
+    var audioPlayerWrap = $('.audio-wrapper'),
+        audioPlayer = $(audioPlayerWrap).find('.audio-player'),
+        audioTitle = $(audioPlayer).find('.audio-player--title'),
+        audio = $('#bgAudio')[0];
+
+    // Audio toggle
+    $(audioPlayer).click(function () {
+      if(audio.paused) {
+        startAudio();
+      } else {
+        audio.pause();
+        $(audioPlayer).removeClass('playing');
+        $(audioTitle).html('вык');
+      }
+    });
+    // Start Audio
+    function startAudio () {
+      audio.volume = 0.5;
+      audio.play();
+      $(audioPlayer).addClass('playing');
+      $(audioTitle).html('вкл');
+    };
+
+    function aboutFn() {
+      setTimeout(function () {
+        contactsIn();
+      }, 1000);
+      setContent('about');
+      console.log('asdf');
+    };
+    
+    var contacts = $('.contacts'),
+        hero = $('.brand-wrap');
+
+    TweenMax.set($(contacts).children(), {
+      x: 200
+    });
+
+    TweenMax.set($(hero).children(), {
+      y: 100,
+      opacity: 0
+    });
+
+    function contactsIn () {
+      setTimeout(function () {
+        TweenMax.staggerTo($(hero).children(), 0.5, {
+          y:0,
+          opacity: 1,
+          ease: Power4.easeIn
+        }, 0.1);
+      }, 300);
+
+      setTimeout(function () {
+        TweenMax.staggerTo($(contacts).children(), 0.3, {
+          x:0,
+          ease: Power1.easeIn
+        }, 0.2);
+      }, 600);      
+    };
+
+
+
+
+    // Fix from starting here
+
+
+    var bg = $('.menu-wrap').children()[0],
+        bgImg = $('.white-overlay'),
+        skewP = $('.global-overlay'),
+        nurik = $('.menu-n'),
+        logo = $('.menu-logo'),
+        callBtn = $('.call-sm'),
+        nav = $('.main-nav-wrap'),
+        ul = $('.main-nav');
+
+    function openMenu(openMenu) {
+      if(openMenu) {
+        var tl = new TimelineLite();
+        tl.to(bg, 0.2, {
+          opacity: 1,
+          visibility: 'visible',
+          ease: Back.easeIn
+        })
+          .to(skewP, 0.3, {
+          x: '0%',
+          ease: Back.easeIn
+        })
+          .to(bgImg, 0.4, {
+            backgroundImage: "url('../assets/images/menu-bg.jpg')",
+            opacity: 1,
+            ease: Power3.easeIn
+          },'-=0.2')
+          .to(nurik,0.5, {
+            opacity: 1,
+            visibility: "visible",
+            ease: Expo.easeIn,
+            onComplete: function () {
+              TweenMax.staggerTo($(ul).children(), 0.2, {
+                visibility: 'visible',
+                opacity: 1,
+                x:'0',
+                ease: Power1.easeIn
+              }, 0.1);
+            }
+          })
+          .to(logo, 0.3, {
+            opacity: 1,
+            y: '0%',
+            ease: Expo.easeIn
+          }, '-=0.2')
+          .to(callBtn, 0.3, {
+            scale: 1,
+            ease: Expo.easeIn,
+            onComplete: function () {
+              callParal(nurik[0]);
+            }
+          },'-=0.1');
+      } else {
+        var tl = new TimelineLite();
+        tl.to(logo, 0.3, {
+          opacity:0,
+          y:'-100%',
+          ease:Expo.easeOut,
+          onComplete: function () {
+            var li = $(ul).children();
+            TweenMax.staggerTo(li, 0.2, {
+              opacity: 0,
+              x:'100',
+              ease: Power1.easeIn
+            }, 0.1);
+            for(var i=0; i< $(li).length; i++) {
+              TweenMax.set($(li)[i], {
+                visibility: 'hidden'
+              })
+            }
+          }
+        })
+          .to(callBtn, 0.3, {
+            scale: 0,
+            ease: Expo.easeOut
+          },'-=0.1')
+          .to(nurik,0.5, {
+          opacity: 0,
+          ease: Expo.easeOut
+        }, '-=0.1')
+          .to(bgImg, 0.4, {
+            opacity: 0,
+            ease: Power3.easeOut
+        },'-=0.2')
+          .to(skewP, 0.5, {
+            x: '100%',
+            ease: Back.easeOut
+        })
+          .to(bg, 0.3, {
+            opacity: 0,
+            ease: Back.easeOut,
+            onComplete: function () {
+              $(bg).css('visibility', 'hidden');
+              $(nurik).css('visibility', 'hidden');
+              offParal();
+            }
+        });
+      }
+    }
+
+    function offParal() {
+      document.onmousemove = '';
+    }
+
+
+    function callParal(el) {
+
+      // Set-up to use getMouseXY function onMouseMove
+      document.onmousemove = getMouseXY;
+
+      // Temporary variables to hold mouse x-y pos.s
+      var tempX = 0;
+      var tempY = 0;
+
+
+      var objectArray = new Array();
+
+      fillObjectArray();
+      positionDivs();
+
+      function fillObjectArray()
+      {
+        var birdDiv = $(el).children()[1];
+        var birdX = 212; //position div from half width of the page
+        var birdY = 33;
+        var birdFactor = 0.05; //parallax shift factor, the bigger the value, the more it shift for parallax movement
+        var birdArray = new Array();
+        birdArray.push(birdDiv, birdX, birdY, birdFactor);
+        objectArray.push(birdArray);
+
+        //var bush1Div = document.getElementById("bush1");
+        //var bush1X = -28;
+        //var bush1Y = 352;
+        //var bush1Factor = 0.06;
+        //var bush1Array = new Array();
+        //bush1Array.push(bush1Div, bush1X, bush1Y, bush1Factor);
+        //objectArray.push(bush1Array);
+
+
+      }
+
+      // Main function to retrieve mouse x-y pos.s
+
+      function getMouseXY(e)
+      {
+
+        // grab the x-y pos.s if browser is NS
+        tempX = e.pageX;
+        tempY = e.pageY;
+
+        // catch possible negative values in NS4
+        if (tempX < 0){tempX = 0}
+        if (tempY < 0){tempY = 0}
+
+        moveDiv(tempX);
+
+        return true
+      }
+
+      function moveDiv(tempX)
+      {
+
+        for (var i=0;i<objectArray.length;i++)
+        {
+          var yourDivPositionX = objectArray[i][3] * (0.5 * 1365 - tempX) + objectArray[i][1];
+          objectArray[i][0].style.left = yourDivPositionX + 'px';
+          console.log(yourDivPositionX);
+        }
+      }
+
+      function positionDivs() {
+        for (var i=0;i<objectArray.length;i++)
+        {
+          objectArray[i][0].style.left = objectArray[i][1] + "px";
+        }
+      }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
     // Initialize
-    var init = function () {
+    function init() {
       routing('hash');
       navigation();
+      menuTrigger();
+      //startAudio();
+      setTimeout(function () {
+        loader.hide();
+      }, 0);
     };
+
+
+
 
     window.onload = init;
 
 })(jQuery);
 
+
+
+
+
+    
--- index.php
+++ index.php
@@ -22,18 +22,138 @@
     <!--[if lt IE 10]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
-    
-    <div class="container">
-      <h1>Майназаров</h1>
-      <nav>
-        <a href="javascript:void(0);" data-switchto="about">About</a>
-        <a href="javascript:void(0);" data-switchto="services">Services</a>
-        <a href="javascript:void(0);" data-switchto="services/corporate">Services Corporate</a>
-        <a href="javascript:void(0);" data-switchto="team">Team</a>
-        <a href="javascript:void(0);" data-switchto="testimonials">Testimonials</a>
-      </nav>
-      <div class="js-content content"></div>
+    <div class="m-app-loading">
+      <!-- BEGIN: Actual animated container. -->
+      <div class="animated-container">
+        <div class="load-wrap">
+          <div class="loader-circle"></div>
+          <div class="loader-line-mask">
+            <div class="loader-line"></div>
+          </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70">
+          <path d="M10.4,10.3c0.9-1.1,2.1-2,3.2-2.9c5.6-4.3,12.6-7,19.7-7.3C42-0.4,50.9,2.6,57.6,8.4c0.7,0.6,1.9,1.7,1.9,1.7L35,60L10.4,10.3z M66.5,50.2l0.8-1.7c5.2-12.1,2.7-27.2-6.4-36.9L66.5,50.2z M8.9,11.9L8.9,11.9c-1.1,1.1-2,2.3-2.8,3.5c-5,7.2-7,16.4-5.6,25c0.4,3.2,1.5,6.4,2.8,9.4l0.2,0.4L8.9,11.9z M12.4,27.2l-0.1,0c-1.2,9.5-2.6,19-3.9,28.6c-0.1,0.6-0.3,1.8-0.3,1.8l0.8,0.9c0,0,2.8,2.7,4.2,4c3.4,2.7,7.2,4.7,11.3,6c2.9,0.8,6,1.6,9.1,1.5L12.4,27.2z M36.6,69.9c9.6-0.2,18.9-4.8,25.1-12.2c-0.8-5.5-1.5-11.1-2.3-16.6c-0.6-4.5-1.3-9-1.8-13.6c-0.1,0-0.2,0-0.2,0L36.6,69.9z"/>
+        </svg>
+      </div>
+      <!-- END: Actual animated container. -->
     </div>
+    <!-- END: App-Loading Screen. -->
+
+    
+    <div class="main-content">
+      <div class="m-video-wrap">
+        <video autoplay poster="assets/images/poster.jpg" id="bgvid" loop>
+          <source src="assets/media/mainazarov.webm" type="video/webm">
+          <source src="assets/media/mainazarov.mp4" type="video/mp4">
+        </video>
+        <div class="video-pattern"></div>
+      </div>
+
+      <div class="brand-wrap text-center" animate-hero>
+        <img class="logo" src="assets/images/logo-lg.svg" alt="">
+        <div class="brand-desc">Профессиональный конферансье, теле-радио ведущий, организатор разного рода мероприятий, резидент лиги Тамашоу и КВН</div>
+        <div class="brand-slogan">Успех Вашего мероприятия в наших руках!</div>
+        <div>
+          <div class="home-call" call-hover ng-click="main.pausePlay = !main.pausePlay"><span>Заказать<br>звонок</span></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <nav>
+      <a href="javascript:void(0);" data-switchto="about">About</a>
+      <a href="javascript:void(0);" data-switchto="services">Services</a>
+      <a href="javascript:void(0);" data-switchto="services/corporate">Services Corporate</a>
+      <a href="javascript:void(0);" data-switchto="team">Team</a>
+      <a href="javascript:void(0);" data-switchto="testimonials">Testimonials</a>
+    </nav> -->
+
+    <div class="burger"></div>
+
+    <div class="audio-wrapper">
+      <audio id="bgAudio" src="assets/media/bg-audio.mp3" preload="auto" controls loop></audio>
+      <div class="audio-player">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <span class="audio-player--title">вык</span>
+      </div>
+    </div>
+    
+
+    <div class="contacts">
+      <div class="tel">+(996) 773 777 555<br>+(996) 554 999 223</div>
+      <div class="address">ул. Советская 2, Здание Хан,<br>3 этаж, г. Бишкек</div>
+      <ul class="social-icons">
+        <li>
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <path d="M 7 5 C 5.9069372 5 5 5.9069372 5 7 L 5 25 C 5 26.093063 5.9069372 27 7 27 L 25 27 C 26.093063 27 27 26.093063 27 25 L 27 7 C 27 5.9069372 26.093063 5 25 5 L 7 5 z M 7 7 L 25 7 L 25 25 L 19.8125 25 L 19.8125 18.25 L 22.40625 18.25 L 22.78125 15.25 L 19.8125 15.25 L 19.8125 13.3125 C 19.8125 12.4385 20.02825 11.84375 21.28125 11.84375 L 22.90625 11.84375 L 22.90625 9.125 C 22.62925 9.088 21.6665 9.03125 20.5625 9.03125 C 18.2585 9.03125 16.6875 10.417 16.6875 13 L 16.6875 15.25 L 14.0625 15.25 L 14.0625 18.25 L 16.6875 18.25 L 16.6875 25 L 7 25 L 7 7 z" color="#000" overflow="visible" font-family="Bitstream Vera Sans"></path>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <path d="M 8 5 C 6.3550302 5 5 6.3550302 5 8 L 5 24 C 5 25.64497 6.3550302 27 8 27 L 24 27 C 25.64497 27 27 25.64497 27 24 L 27 8 C 27 6.3550302 25.64497 5 24 5 L 8 5 z M 21 8 L 24 8 L 24 11 L 21 11 L 21 8 z M 16 12 C 18.220984 12 20 13.779016 20 16 C 20 18.220984 18.220984 20 16 20 C 13.779016 20 12 18.220984 12 16 C 12 13.779016 13.779016 12 16 12 z M 7 14 L 10.34375 14 C 10.122261 14.624412 10 15.301793 10 16 C 10 19.301864 12.698136 22 16 22 C 19.301864 22 22 19.301864 22 16 C 22 15.301793 21.877739 14.624412 21.65625 14 L 25 14 L 25 24 C 25 24.56503 24.56503 25 24 25 L 8 25 C 7.4349698 25 7 24.56503 7 24 L 7 14 z" color="#000" overflow="visible" font-family="Bitstream Vera Sans"></path>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <path d="M28,8.557c-0.883,0.392-1.832,0.656-2.828,0.775c1.017-0.609,1.797-1.574,2.165-2.724c-0.951,0.564-2.005,0.974-3.127,1.195 c-0.898-0.957-2.178-1.555-3.594-1.555c-2.719,0-4.924,2.205-4.924,4.924c0,0.386,0.044,0.762,0.127,1.122 c-4.092-0.205-7.72-2.166-10.149-5.145C5.247,7.876,5.004,8.722,5.004,9.625c0,1.708,0.869,3.215,2.19,4.098 c-0.807-0.026-1.566-0.247-2.23-0.616c0,0.021,0,0.041,0,0.062c0,2.386,1.697,4.376,3.95,4.828C8.501,18.11,8.066,18.17,7.616,18.17 c-0.317,0-0.626-0.031-0.926-0.088c0.627,1.956,2.445,3.38,4.6,3.42c-1.685,1.321-3.808,2.108-6.115,2.108 c-0.397,0-0.789-0.023-1.175-0.069c2.179,1.397,4.767,2.212,7.548,2.212c9.057,0,14.009-7.503,14.009-14.01 c0-0.213-0.005-0.426-0.014-0.637C26.505,10.411,27.34,9.544,28,8.557z"></path>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <path d="M 12.96875 4 C 6.58075 4 5.5625 8.4545 5.5625 9.9375 C 5.5625 12.1455 7.277 14.90625 10.75 14.90625 C 11.066 14.90625 11.42425 14.88475 11.78125 14.84375 C 11.72225 15.00875 11.46875 15.5535 11.46875 16.0625 C 11.46875 17.1225 12.14 17.80575 12.375 18.21875 C 8.575 18.18175 4 19.833 4 23.5 C 4 24.842 5.1485 28 10.5625 28 C 16.7375 28 18.625 24.1995 18.625 22.0625 C 18.625 17.9805 14.3125 17.29125 14.3125 15.40625 C 14.3125 13.42825 17.53125 13.26725 17.53125 9.40625 C 17.53125 6.60925 15.572 5.248 15.375 5.125 L 17.3125 5.125 L 19.25 4 L 12.96875 4 z M 11.03125 5.03125 C 14.10925 5.03125 14.875 9.60175 14.875 10.84375 C 14.875 11.13075 15.06975 14.03125 12.21875 14.03125 C 10.05675 14.03125 8.21875 11.31225 8.21875 8.40625 C 8.21875 7.54325 8.64225 5.03125 11.03125 5.03125 z M 24 11 L 24 14 L 21 14 L 21 16 L 24 16 L 24 19 L 26 19 L 26 16 L 29 16 L 29 14 L 26 14 L 26 11 L 24 11 z M 12.53125 19.09375 C 12.80125 19.09375 13.01875 19.104 13.21875 19.125 C 14.31075 19.941 16.59375 21.10825 16.59375 23.28125 C 16.59275 24.26625 16.0045 26.6875 12.0625 26.6875 C 10.0835 26.6875 6.65625 25.84275 6.65625 22.84375 C 6.65625 21.80675 7.27625 19.09375 12.53125 19.09375 z"></path>
+            </svg>
+          </a>
+        </li>
+      </ul>
+      <a href="mailto:info@mainazarov.kg" class="email">info@mainazarov.kg</a>
+      <div class="copy">&copy; Mainazarovteam - 2016</div>
+    </div>
+
+    <div class="menu-wrap">
+      <div class="white-overlay">
+        <div class="menu-img"></div>
+      </div>
+      <div class="menu-n">
+        <img src="../assets/images/menu-n.png" alt="">
+      </div>
+      <div class="global-overlay">
+        <div class="overlay skew-part">
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
+        </div>
+      </div>
+      <div class="menu-logo">
+        <img src="../assets/images/logo-hor.svg" alt="">
+      </div>
+      <div class="call-sm" call-hover-sm>
+        <span>Заказать<br>звонок</span>
+      </div>
+      <div class="main-nav-wrap">
+        <ul class="main-nav">
+          <li><a href="#">На главную</a></li>
+          <li><a href="#">Майназаров Н.</a></li>
+          <li><a href="#">Услуги</a></li>
+          <li><a href="#">Команда</a></li>
+          <li><a href="#">Отзывы</a></li>
+          <li><a href="#">Галерея</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </div>
+    </div>
+  
+
+    
     
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.
     <script>
@@ -46,6 +166,8 @@
     </script> -->
     <script src="<?php echo $root; ?>assets/js/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo $root; ?>assets/js/vendor/navigo/navigo.min.js"></script>
+    <script src="<?php echo $root; ?>assets/js/vendor/gsap/TweenMax.min.js"></script>
+    <script src="<?php echo $root; ?>assets/js/vendor/gsap/TimelineLite.min.js"></script>
 
     
     
